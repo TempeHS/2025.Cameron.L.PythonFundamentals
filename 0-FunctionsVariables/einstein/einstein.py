9d4b686c7b5a26a5db8f61fdf3a27deb31383a8d
cameronly08 <124547283+cameronly08@users.noreply.github.com>
2024-02-08 09:41:32 +0000

    Completed einstein.py

---
+++ 0-FunctionsVariables/einstein/einstein.py
@@ -0,0 +1,10 @@
+# Prompts user for input
+mass = int(input("Enter mass to convert into energy:"))
+
+speed = 300000000
+
+# Converts int into joules
+einstein = mass * speed**2
+
+# Print output
+print("The Equivalent energy is:", einstein)
