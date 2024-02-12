e95a5b49b9ce79a9f082bc7aa02edc2cd0641a8f
cameronly08 <124547283+cameronly08@users.noreply.github.com>
2024-02-12 00:28:12 +0000

    Upload completed deep.py

--- 1-Conditionals/deep/deep.py
+++ 1-Conditionals/deep/deep.py
@@ -1,13 +1,11 @@
-def main():
-    x = input("What is the answer to the Great Question in life, the Universe and Everything?")
-if x == int(input("42")):
+x = input(
+    "What is the answer to the Great Question in life, the Universe and Everything?"
+)
+if x == (" 42"):
     print("Yes")
-elif x == input("fourty-two"):
+elif x == (" fourty-two"):
     print("Yes")
-elif x == input("fourty two"):
+elif x == (" Fourty two"):
     print("Yes")
 else:
     print("No")
-
-
-main()
