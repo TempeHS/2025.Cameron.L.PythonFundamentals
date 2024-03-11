fbf8367aacd5a5bf396af90b239d8b8b9292f772
cameronly08 <124547283+cameronly08@users.noreply.github.com>
2024-03-11 01:49:20 +0000

    Professor in progress

---
+++ 4-Libraries/professor/professor.py
@@ -0,0 +1,24 @@
+import random
+
+def main():
+    
+
+
+def get_level():
+    user_input = int(input("Level:"))
+    level = random.randint(1, 3)
+        
+
+
+
+def generate_integer(level):
+    while True:
+        try:
+            if get
+    
+
+
+
+
+if __name__ == "__main__":
+main()
