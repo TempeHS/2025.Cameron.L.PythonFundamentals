ffbde71f6d12d46407db4f481b33567db7627181
cameronly08 <124547283+cameronly08@users.noreply.github.com>
2024-03-16 03:54:58 +0000

    Completed

--- 4-Libraries/bitcoin/bitcoin.py
+++ 4-Libraries/bitcoin/bitcoin.py
@@ -1,13 +1,28 @@
 import sys
 import requests
 
-try:
-    r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    request = r.json()
-    bitcoin = 
-except requests.RequestException:
-    sys.exit
-    print("Missing command-line argument")
-except ValueError:
-    sys.exit
-    print("Command-line argument is not a number")
+
+def main():
+    if len(sys.argv) != 2:
+        print("Missing command-line argument")
+        sys.exit(1)
+
+    try:
+        value = float(sys.argv[1])
+    except ValueError:
+        print("Command-line argument is not a number")
+        sys.exit(1)
+
+    try:
+        r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        request = r.json()
+        bitcoin = request["bpi"]["USD"]["rate_float"]
+        amount = bitcoin * value
+        print(f"${amount:,.4f}")
+    except requests.RequestException:
+        print("Failed to get Bitcoin price")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
