9b4b304b16f4b1947b897f3b75e2e313a79f056e
cameronly08 <124547283+cameronly08@users.noreply.github.com>
2024-03-17 05:20:08 +0000

    In progress

--- 4-Libraries/professor/professor.py
+++ 4-Libraries/professor/professor.py
@@ -2,17 +2,20 @@ import random
 
 
 def main():
+    x = random.randint(1, 9)
+    y = random.randint(1, 9)
+    answer = x + y
 
 
-def get_level():
-    user_input = int(input("Level:"))
-    level = random.randint(1, 3)
+    def get_level():
+        user_input = int(input("Level:"))
+        level = random.randint(1, 3)
 
 
-def generate_integer(level):
-    if 1 > get_level > 3:
-        print("Invalid")
-    raise ValueError
+    def generate_integer(level):
+        if 1 > get_level > 3:
+            print("Invalid")
+        raise ValueError
 
 
 if __name__ == "__main__":
